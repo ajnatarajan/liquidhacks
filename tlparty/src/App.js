413f071169befda41a5d9975356742e4e1b47125
ajnatarajan <ajnatarajan@gmail.com>
2021-11-08 10:39:56 -0800

    profile page done- added EventPreviewSection and ProfilePage and UserInformation components

--- tlparty/src/App.js
+++ tlparty/src/App.js
@@ -2,7 +2,8 @@ import logo from './assets/logo.svg';
 import { Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
-import EventPreview from './components/EventPreview'
+import ProfilePage from './components/ProfilePage';
+import EventPreview from './components/EventPreview';
 
 function App() {
   return (

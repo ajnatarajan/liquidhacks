c8764998d24cacc51eb7b95ebf9ca408a98b7078
ptes77 <31578309+ptes77@users.noreply.github.com>
2021-11-07 23:17:25 -0800

    Adding temp work for host flow

--- tlparty/src/routes/Host.js
+++ tlparty/src/routes/Host.js
@@ -1,17 +1,137 @@
 import logo from '../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import {
+  Link,
+  useHistory,
+} from 'react-router-dom';
 import './Host.css';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Host() {
+  const [fields, setFields] = useState({
+    eventName: "",
+    eventLocation: "",
+    eventTime: "", // TODO: Change to datetime format
+    eventDate: "", // TODO: Change to datetime format
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+  const [showForm, setShowForm] = useState(true); // true for debugging purpose
+  const [isLoading, setIsLoading] = useState(false);
+
+  /*
+  function validateForm() {
+    return (
+      fields.email.length > 0 &&
+      fields.password.length > 0 &&
+      fields.password === fields.confirmPassword
+    );
+  }
+
+  function validateConfirmationForm() {
+    return fields.confirmationCode.length > 0;
+  }
+  */
+
+  function handleHostButtonClick() {
+    console.log(showForm);
+    setShowForm(!showForm);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    // setNewUser("test");
+
+    setIsLoading(false);
+  }
+
+  function renderForm() {
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className='mb-3 mx-3' controlId="eventName">
+          <Form.Label>Event Name</Form.Label>
+          <Form.Control
+            autoFocus
+            type="text"
+            placeholder="Enter Event Name"
+            value={fields.eventName}
+            onChange={setFields}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3 mx-3' controlId="eventLocation">
+          <Form.Label>Event Location</Form.Label>
+          <Form.Control
+            autoFocus
+            type="text"
+            value={fields.eventLocation}
+            onChange={setFields}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3 mx-3' controlId="eventDate">
+          <Form.Label>Event Date</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={fields.eventDate}
+            onChange={setFields}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3 mx-3' controlId="eventTime">
+          <Form.Label>Event Time</Form.Label>
+          <Form.Control
+            type="password"
+            value={fields.eventTime}
+            onChange={setFields}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3 mx-3' controlId="firstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={handleFieldChange}
+            value={fields.firstName}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3 mx-3' controlId="lastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={handleFieldChange}
+            value={fields.lastName}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3 mx-3' controlId="firstName">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            onChange={handleFieldChange}
+            value={fields.email}
+          />
+        </Form.Group>
+      </Form>
+    );
+  }
+  
   return (
     <div className="Host">
-      <header className="Host-header">
+      <div className="Host-header">
         <img src={logo} className="Host-logo" alt="logo" />
         <p>
           Currently on <code>src/routes/Host.js</code>.
         </p>
+        <button
+          type="button"
+          className="host-button"
+          onClick={handleHostButtonClick}>
+            Click me!
+        </button>
         <Link to='/' className='Host-link'>Return home</Link>
-      </header>
+      </div>
+      {showForm ? renderForm() : null}
     </div>
   );
 }

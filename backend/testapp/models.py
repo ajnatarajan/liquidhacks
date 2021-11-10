ffe3e14afe747845c86d220b721a4f7910e1f3b8
ptes77 <31578309+ptes77@users.noreply.github.com>
2021-11-10 12:01:23 -0800

    Initial db setup

--- backend/testapp/models.py
+++ backend/testapp/models.py
@@ -11,21 +11,53 @@ from django.contrib.postgres.fields import ArrayField
     lastName: '',
     email: '',
     terms: false,
+
+    event_id (primary key)
+    event_name
+    location
+    game (from Liquipedia DB)
+    video_game
+    image
+    num_attendees
+    date_time
+    timezone
+    vibes
+    snacks
+    contact_firstname
+    contact_lastname
+    contact_email
+
 '''
 
 class Event(models.Model):
-    GAME_CHOICES = ['Valorant', 'League', 'CSGO', 'Dota', 'PUBG', 'Smash']
+    VALORANT = 'VAL'
+    LEAGUE = 'LOL'
+    CSGO = 'CSG'
+    DOTA = 'DOT'
+    PUBG = 'PUB'
+    SMASH = 'SMA'
+    GAME_CHOICES = [
+        (VALORANT, 'Valorant'),
+        (LEAGUE, 'League'),
+        (CSGO, 'CSGO'),
+        (DOTA, 'Dota'),
+        (PUBG, 'PUBG'),
+        (SMASH, 'Smash')
+    ]
     
-    event_name = models.CharField()
-    event_location = models.CharField()
+    event_id = models.AutoField(primary_key=True)
+    event_name = models.CharField(max_length=100)
+    event_location = models.CharField(max_length=100)
     event_datetime = models.DateTimeField()
     event_vibes = ArrayField(
-        models.CharField()
+        models.CharField(max_length=50),
+        default=list,
     )
-    host_email=models.EmailField()
-    host_first_name = models.CharField()
-    host_last_name = models.CharField()
+    host_email=models.EmailField(max_length=50)
+    host_first_name = models.CharField(max_length=50)
+    host_last_name = models.CharField(max_length=50)
     num_attendees = models.IntegerField()
     game_type = models.CharField(
-        choices=GAME_CHOICES
+        max_length=3,
+        choices=GAME_CHOICES,
     )

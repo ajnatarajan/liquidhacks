0d4badfd75468b8d6af9a3f3c9a22587bc50cae4
SuperMario210 <Sup3rMar1o210@gmail.com>
2021-11-10 18:59:29 -0800

    fix profile page styling

--- tlparty/src/components/ProfilePage.js
+++ tlparty/src/components/ProfilePage.js
@@ -16,11 +16,11 @@ export default function ProfilePage(props) {
         return <Landing />
     }
     return (
-        <div>
+        <div class="page-background-theme" style={{minHeight: '100vh'}}>
             {/* if you change returnTo, talk to Ajay. He needs to change something
             in his auth0 account otherwise this will break*/}
             <TopBar button_text="PROFILE" on_click={goToProfile} button_text_2="LOG OUT" on_click_2={() => logout({ returnTo: "http://localhost:3000" })}/>
-            <div class="page-background-theme">
+            <div>
                 {/* <UserInformation
                     title="Profile"
                     submit_text="Update Information"

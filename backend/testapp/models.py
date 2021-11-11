25c9e87cba35f7f44940146b1f4883e3a2009a89
ptes77 <31578309+ptes77@users.noreply.github.com>
2021-11-10 22:58:29 -0800

    Database set up, still need to figure out api and react integration

--- backend/testapp/models.py
+++ backend/testapp/models.py
@@ -1,63 +1,65 @@
-from django.db import models
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.contrib.postgres.fields import ArrayField
+from django.db import models
 
-# Create your models here.
-'''
-    eventName: '',
-    eventLocation: '',
-    eventDateTime: new Date(), // TODO: Change to datetime format
-    eventVibes: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    terms: false,
-
-    event_id (primary key)
-    event_name
-    location
-    game (from Liquipedia DB)
-    video_game
-    image
-    num_attendees
-    date_time
-    timezone
-    vibes
-    snacks
-    contact_firstname
-    contact_lastname
-    contact_email
-
-'''
 
 class Event(models.Model):
-    VALORANT = 'VAL'
-    LEAGUE = 'LOL'
-    CSGO = 'CSG'
-    DOTA = 'DOT'
-    PUBG = 'PUB'
-    SMASH = 'SMA'
-    GAME_CHOICES = [
-        (VALORANT, 'Valorant'),
-        (LEAGUE, 'League'),
-        (CSGO, 'CSGO'),
-        (DOTA, 'Dota'),
-        (PUBG, 'PUBG'),
-        (SMASH, 'Smash')
-    ]
-    
-    event_id = models.AutoField(primary_key=True)
+    event_id = models.UUIDField(primary_key=True)
     event_name = models.CharField(max_length=100)
-    event_location = models.CharField(max_length=100)
-    event_datetime = models.DateTimeField()
-    event_vibes = ArrayField(
+    location = models.CharField(max_length=100)
+    game = models.TextField()
+    video_game = models.CharField(max_length=50)
+    image = models.BinaryField()
+    num_attendees = models.IntegerField()
+    date_time = models.TimeField()
+    timezone = models.CharField(max_length=10)
+    vibes = ArrayField(
         models.CharField(max_length=50),
         default=list,
     )
-    host_email=models.EmailField(max_length=50)
-    host_first_name = models.CharField(max_length=50)
-    host_last_name = models.CharField(max_length=50)
-    num_attendees = models.IntegerField()
-    game_type = models.CharField(
-        max_length=3,
-        choices=GAME_CHOICES,
+    snacks = ArrayField(
+        models.CharField(max_length=50),
+        default=list,
     )
+    contact_firstname = models.CharField(max_length=50)
+    contact_lastname = models.CharField(max_length=50)
+    contact_email = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'event'
+    
+    def __str__(self):
+        return self.event_name
+
+
+class PastEvent(models.Model):
+    email_address = models.CharField(primary_key=True, max_length=100)
+    event_id = models.ForeignKey("Event", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'past_event'
+
+
+class UpcomingEvent(models.Model):
+    email_address = models.CharField(primary_key=True, max_length=100)
+    event_id = models.ForeignKey("Event", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'upcoming_event'
+
+
+class User(models.Model):
+    email_address = models.CharField(primary_key=True, max_length=100)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    phone_number = models.CharField(max_length=50, blank=True, null=True)
+    is_vaccinated = models.BooleanField()
+
+    class Meta:
+        db_table = 'user'
--- backend/testapp/views.py
+++ backend/testapp/views.py
@@ -1,3 +1,58 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from testapp.models import Event, PastEvent, UpcomingEvent, User
+from testapp.serializers import (
+    EventSerializer,
+    PastEventSerializer,
+    UpcomingEventSerializer,
+    UserSerializer,
+)
+from rest_framework import generics
 
 # Create your views here.
+
+class EventListCreate(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class PastEventListCreate(generics.ListCreateAPIView):
+    queryset = PastEvent.objects.all()
+    serializer_class = PastEventSerializer
+
+
+class UpcomingEventListCreate(generics.ListCreateAPIView):
+    queryset = UpcomingEvent.objects.all()
+    serializer_class = UpcomingEventSerializer
+
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the testapp index. Yay!")
+
+def detail(request, email_address):
+    return HttpResponse("You're looking at user %s." % email_address)
+
+def results(request, email_address):
+    response = "You're looking at the results of user %s."
+    return HttpResponse(response % email_address)
+
+def vote(request, email_address):
+    return HttpResponse("You're voting on user %s." % email_address)
+
+
+def user_detail(request, pk):
+    user = User.objects.get(pk=pk)
+    events = Event.objects.filter(pk=user.email_address)
+
+    context = {
+        "user": user,
+        "events": events,
+    }
+
+    return render(request, "car_detail.html", context)
+

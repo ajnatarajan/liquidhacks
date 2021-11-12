6111922d2687b4863330e4c52e7a4c7355c623d4
ptes77 <31578309+ptes77@users.noreply.github.com>
2021-11-12 15:31:11 -0800

    changed index site location

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('users/<email_address>/', views.detail, name='detail'),
     path('all/', views.all, name='all'),
     path('omg/', views.omg, name='omg'),

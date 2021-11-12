05340b5fba5c656fc967034be39193f304b96ef6
ajnatarajan <ajnatarajan@gmail.com>
2021-11-11 18:07:16 -0800

    event preview --> pop up modal with data finished, dates also formatted better

--- tlparty/src/components/EventPreview.js
+++ tlparty/src/components/EventPreview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './EventPreview.css'
 import tl_background_old from '../img/tl_background_old.jpg';
@@ -10,6 +10,8 @@ import dota_icon from '../img/dota_icon.png';
 import rainbowsix_icon from '../img/rainbowsix_icon.png';
 import starcraft_icon from '../img/starcraft_icon.png';
 import attendee_icon from '../img/attendee_icon.png';
+import Modal from './Modal';
+import EventModal from './EventModal';
 
 export const GameIcon = (props) => {
     let Output;
@@ -54,25 +56,68 @@ export const EventPic = (props) => {
     return Output;
 };
 
+
 export default function EventPreview(props) {
+    function openModal() {
+        setIsModalOpen(!isModalOpen);
+    }
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const { full_event } = props;
+    console.log("LETS SEE IF I HAVE ACCESS: ", full_event);
+
+    const dbGameCodeToEnglish = {
+        "leagueoflegends": "League of Legends",
+        "valorant": "Valorant",
+        "dota2": "Dota 2",
+        "starcraft2": "Starcraft 2",
+        "counterstrike": "Counter-Strike: Global Offensive",
+        "rainbowsix": "Rainbow Six",
+    }
+
+    function dateToDayMonth(dateString) {
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const d = new Date(dateString);
+        var dayName = days[d.getDay()];
+        var month = months[d.getMonth()];
+        var date = d.getDate();
+        return dayName + " " + month + " " + date;
+    }
+
+
     return (
-    <a href="#" class="event-preview-clickable-region">
-        <span style={{display: "inline-block"}} class="square">
-            <EventPic {...props}/>
-            <div class="event-preview-title-text">
-                {props.title}
-            </div>
-            <div class="box black-opaque-box">
-                <div class="people-box">
-                    <img src={attendee_icon} class="event-game-icon"/>
-                    {props.num_attendees}
+        <div>
+            <div class="event-preview-clickable-region" style={{cursor: "pointer"}} onClick={openModal}>
+            <span style={{display: "inline-block"}} class="square">
+                <EventPic {...props}/>
+                <div class="event-preview-title-text">
+                    {props.title}
                 </div>
-                <GameIcon {...props} />
-                <div class="event-time">
-                    {props.date}
+                <div class="box black-opaque-box">
+                    <div class="people-box">
+                        <img src={attendee_icon} class="event-game-icon"/>
+                        {props.num_attendees}
+                    </div>
+                    <GameIcon {...props} />
+                    <div class="event-time">
+                        {dateToDayMonth(full_event["date_time"])}
+                    </div>
                 </div>
+            </span>
             </div>
-        </span>
-    </a>
+            <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen}>
+                <EventModal
+                    name={full_event["event_name"]}
+                    host={full_event["contact_firstname"] + " " + full_event["contact_lastname"]}
+                    liquipedia_pagename={full_event["game"]}
+                    game={dbGameCodeToEnglish[full_event["video_game"]]}
+                    location={full_event["location"]}
+                    date={full_event["date_time"]}
+                    vibes={full_event["vibes"]}
+                    snacks={full_event["snacks"]}
+                />
+            </Modal>
+        </div>
     );
 }

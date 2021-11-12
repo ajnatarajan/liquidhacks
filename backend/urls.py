917a7b93394034e85acad24979a90873a7f9fcaf
ptes77 <31578309+ptes77@users.noreply.github.com>
2021-11-12 14:43:41 -0800

    Added frontend routing

--- backend/urls.py
+++ backend/urls.py
@@ -18,9 +18,26 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from testapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('testapp.urls')),
+    path('', views.index, name='index'),
+    path('users/<email_address>/', views.detail, name='detail'),
+    path('all/', views.all, name='all'),
+    path('omg/', views.omg, name='omg'),
+    path('getUserEvents/', views.getUserEvents, name='getUserEvents'),
+    path('getPastUserEvents/', views.getPastUserEvents, name='getPastUserEvents'),
+    path('getUpcomingUserEvents/', views.getUpcomingUserEvents, name='getUpcomingUserEvents'),
+    path('getEvent/', views.getEvent, name='getEvent'),
+    path('getAllEvents/', views.getAllEvents, name='getAllEvents'),
+    path('getAllPastEvents/', views.getAllPastEvents, name='getAllPastEvents'),
+    path('getAllUpcomingEvents/', views.getAllUpcomingEvents, name='getAllUpcomingEvents'),
+    path('editEvent/', views.editEvent, name='editEvent'),
+    path('addEvent/', views.addEvent, name='addEvent'),
+    path('addUserEvent/', views.addUserEvent, name='addUserEvent'),
+    path('addUser/', views.addUser, name='addUser'),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
